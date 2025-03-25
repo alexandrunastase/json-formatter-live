e13eda587f759e671391802cfd50d2874bfcea3a
Alexandru Nastase <alexandru.nastase@outlook.com>
2025-03-25 19:08:19 +0100

    Update workflow

--- playwright.config.js
+++ playwright.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
-    baseURL: 'http://localhost:3080',
+    baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
   },
   projects: [
